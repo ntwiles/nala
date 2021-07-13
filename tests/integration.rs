9366ef9004cbb27e2d7c57103f36f10a820e185a
Nathan Wiles <ntwiles@gmail.com>
2021-07-12 22:57:25 -0400

    Rename data/ to example/.

--- tests/integration.rs
+++ tests/integration.rs
@@ -5,7 +5,7 @@ use library::{interpreter::interpret_tree, parser::parse_code, scope::Scope};
 
 #[test]
 fn test_run_all_examples() {
-    let files = fs::read_dir("data").unwrap();
+    let files = fs::read_dir("example").unwrap();
 
     for file in files {
         if let Ok(file) = file {
