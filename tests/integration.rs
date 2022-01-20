681bcc2a66a57c4f3460a5e0b8a270756cff72ca
Nathan Wiles <ntwiles@gmail.com>
2022-01-19 20:49:02 -0500

    Reorder test files.

--- tests/integration.rs
+++ tests/integration.rs
@@ -31,7 +31,7 @@ fn test_run_examples() {
     ];
 
     for (file, expected) in test_data {
-        let file_name = format!("test/{}.nl", file);
+        let file_name = format!("test/output/{}.nl", file);
         let mut test_context = TestContext::new();
         assert_example_does_not_throw(&file_name, &mut test_context);
         assert_eq!(test_context.get_output(), &expected, "{}", file_name);
@@ -50,7 +50,7 @@ fn test_run_input_examples() {
     ];
 
     for (file, inputs, expected) in test_data {
-        let file_name = format!("test/{}.nl", file);
+        let file_name = format!("test/input/{}.nl", file);
 
         let mut test_context = TestContext::new();
         test_context.mock_inputs(inputs);
