622ffb6373abf315e71ae4efd13760ea99b8972d
Nathan Wiles <ntwiles@gmail.com>
2022-01-30 18:45:51 -0500

    Add runtime error test.

--- tests/integration_errors.rs
+++ tests/integration_errors.rs
@@ -1,9 +1,26 @@
 use library::{io_context::TestContext, test_util::read_and_execute};
 
 #[test]
+#[should_panic(expected = "Passed value `7` of type Number to func `len`")]
+fn it_errors_when_passing_number_arg_to_len() {
+    let file_name = "tests/nala/error/runtime/array-len.nl";
+    let mut test_context = TestContext::new();
+    read_and_execute(&file_name, &mut test_context);
+}
+
+#[test]
 #[should_panic(expected = "Cannot assign value of type String where Number is expected.")]
 fn it_errors_when_assigning_wrong_type() {
     let file_name = "tests/nala/error/runtime/assign-types.nl";
+    let mut test_context = TestContext::new();
+
+    read_and_execute(&file_name, &mut test_context);
+}
+
+#[test]
+#[should_panic(expected = "Cannot assign Void.")]
+fn it_errors_when_assigning_type_void() {
+    let file_name = "tests/nala/error/runtime/assign-void.nl";
     let mut test_context = TestContext::new();
 
     read_and_execute(&file_name, &mut test_context);
@@ -17,13 +34,5 @@ fn it_errors_when_passing_primitive_when_nested_is_expected() {
     let file_name = "tests/nala/error/runtime/nested-types.nl";
     let mut test_context = TestContext::new();
 
-    read_and_execute(&file_name, &mut test_context);
-}
-
-#[test]
-#[should_panic(expected = "Passed value `7` of type Number to func `len`")]
-fn it_errors_when_passing_number_arg_to_len() {
-    let file_name = "tests/nala/error/runtime/array-len.nl";
-    let mut test_context = TestContext::new();
     read_and_execute(&file_name, &mut test_context);
 }
