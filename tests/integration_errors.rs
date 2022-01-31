45413edb4e66633f6ffa32c5114e4dc82a52033e
Nathan Wiles <ntwiles@gmail.com>
2022-01-30 23:22:01 -0500

    Simplify integration_errors syntax.

--- tests/integration_errors.rs
+++ tests/integration_errors.rs
@@ -7,50 +7,35 @@ fn test_path(name: &str) -> String {
 #[test]
 #[should_panic(expected = "Cannot index using non-numeric value.")]
 fn it_errors_when_indexing_array_with_string() {
-    let file_name = test_path("array-index-string");
-    let mut test_context = TestContext::new();
-    read_and_execute(&file_name, &mut test_context);
+    read_and_execute(&test_path("array-index-string"), &mut TestContext::new());
 }
 
 #[test]
 #[should_panic(expected = "Passed value `7` of type Number to func `len`")]
 fn it_errors_when_passing_number_arg_to_len() {
-    let file_name = test_path("array-len");
-    let mut test_context = TestContext::new();
-    read_and_execute(&file_name, &mut test_context);
+    read_and_execute(&test_path("array-len"), &mut TestContext::new());
 }
 
 #[test]
 #[should_panic(expected = "Arrays can contain elements of only a single type.")]
 fn it_errors_when_declaring_array_multiple_types() {
-    let file_name = test_path("array-multiple-types");
-    let mut test_context = TestContext::new();
-    read_and_execute(&file_name, &mut test_context);
+    read_and_execute(&test_path("array-multiple-types"), &mut TestContext::new());
 }
 
 #[test]
 #[should_panic(expected = "Cannot assign a value of type String where Number is expected.")]
 fn it_errors_when_assigning_wrong_type() {
-    let file_name = test_path("assign-types");
-    let mut test_context = TestContext::new();
-
-    read_and_execute(&file_name, &mut test_context);
+    read_and_execute(&test_path("assign-types"), &mut TestContext::new());
 }
 
 #[test]
 #[should_panic(expected = "Cannot declare a variable with a value of type Void.")]
 fn it_errors_when_assigning_type_void() {
-    let file_name = test_path("assign-void");
-    let mut test_context = TestContext::new();
-
-    read_and_execute(&file_name, &mut test_context);
+    read_and_execute(&test_path("assign-void"), &mut TestContext::new());
 }
 
 #[test]
 #[should_panic(expected = "Type `Bool` does not support nesting. Type `Bool<String>` is invalid.")]
 fn it_errors_when_passing_primitive_when_nested_is_expected() {
-    let file_name = test_path("nested-types");
-    let mut test_context = TestContext::new();
-
-    read_and_execute(&file_name, &mut test_context);
+    read_and_execute(&test_path("nested-types"), &mut TestContext::new());
 }
