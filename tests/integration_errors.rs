07d9acfa305e84b02ab3620572501a78ac7bb821
Nathan Wiles <ntwiles@gmail.com>
2022-01-30 23:18:44 -0500

    Extract test_path.

--- tests/integration_errors.rs
+++ tests/integration_errors.rs
@@ -1,9 +1,13 @@
 use library::{io_context::TestContext, test_util::read_and_execute};
 
+fn test_path(name: &str) -> String {
+    format!("tests/nala/integration_errors/{}.nl", name)
+}
+
 #[test]
 #[should_panic(expected = "Cannot index using non-numeric value.")]
 fn it_errors_when_indexing_array_with_string() {
-    let file_name = "tests/nala/integration_errors/array-index-string.nl";
+    let file_name = test_path("array-index-string");
     let mut test_context = TestContext::new();
     read_and_execute(&file_name, &mut test_context);
 }
@@ -11,7 +15,7 @@ fn it_errors_when_indexing_array_with_string() {
 #[test]
 #[should_panic(expected = "Passed value `7` of type Number to func `len`")]
 fn it_errors_when_passing_number_arg_to_len() {
-    let file_name = "tests/nala/integration_errors/array-len.nl";
+    let file_name = test_path("array-len");
     let mut test_context = TestContext::new();
     read_and_execute(&file_name, &mut test_context);
 }
@@ -19,7 +23,7 @@ fn it_errors_when_passing_number_arg_to_len() {
 #[test]
 #[should_panic(expected = "Arrays can contain elements of only a single type.")]
 fn it_errors_when_declaring_array_multiple_types() {
-    let file_name = "tests/nala/integration_errors/array-multiple-types.nl";
+    let file_name = test_path("array-multiple-types");
     let mut test_context = TestContext::new();
     read_and_execute(&file_name, &mut test_context);
 }
@@ -27,7 +31,7 @@ fn it_errors_when_declaring_array_multiple_types() {
 #[test]
 #[should_panic(expected = "Cannot assign a value of type String where Number is expected.")]
 fn it_errors_when_assigning_wrong_type() {
-    let file_name = "tests/nala/integration_errors/assign-types.nl";
+    let file_name = test_path("assign-types");
     let mut test_context = TestContext::new();
 
     read_and_execute(&file_name, &mut test_context);
@@ -36,7 +40,7 @@ fn it_errors_when_assigning_wrong_type() {
 #[test]
 #[should_panic(expected = "Cannot declare a variable with a value of type Void.")]
 fn it_errors_when_assigning_type_void() {
-    let file_name = "tests/nala/integration_errors/assign-void.nl";
+    let file_name = test_path("assign-void");
     let mut test_context = TestContext::new();
 
     read_and_execute(&file_name, &mut test_context);
@@ -45,7 +49,7 @@ fn it_errors_when_assigning_type_void() {
 #[test]
 #[should_panic(expected = "Type `Bool` does not support nesting. Type `Bool<String>` is invalid.")]
 fn it_errors_when_passing_primitive_when_nested_is_expected() {
-    let file_name = "tests/nala/integration_errors/nested-types.nl";
+    let file_name = test_path("nested-types");
     let mut test_context = TestContext::new();
 
     read_and_execute(&file_name, &mut test_context);
