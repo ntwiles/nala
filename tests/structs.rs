5855fe1d3125e640362fd667edb344f341e2cb6a
Nathan Wiles <ntwiles@gmail.com>
2023-03-08 00:01:12 -0500

    Add tests for struct declare.

--- tests/structs.rs
+++ tests/structs.rs
@@ -1,1 +1,50 @@
-// TODO: Create tests for structs including making sure that nested struct fields work.
+use nala_interpreter::io_context::TestContext;
+use test_util::parse_and_run;
+
+#[test]
+fn it_runs_basic_struct_declare() {
+    let mut ctx = TestContext::new();
+
+    let nala = r#"
+        struct Foo {
+            bar: String,
+            baz: Number,
+        }
+
+        const foo: Foo = {
+            bar: 'bar',
+            baz: 7
+        };
+
+        print(foo.bar);
+    "#;
+
+    assert!(parse_and_run(nala, &mut ctx).is_ok());
+    assert_eq!(ctx.get_output(), vec!["bar"]);
+}
+
+#[test]
+fn it_runs_nested_struct_declare() {
+    let mut ctx = TestContext::new();
+
+    let nala = r#"
+        struct Foo {
+            bar: String,
+            nested: {
+                baz: Number,
+            }
+        }
+
+        const foo: Foo = {
+            bar: 'bar',
+            nested: {
+                baz: 7
+            }
+        };
+
+        print(foo.nested.baz);
+    "#;
+
+    assert!(parse_and_run(nala, &mut ctx).is_ok());
+    assert_eq!(ctx.get_output(), vec!["7"]);
+}
