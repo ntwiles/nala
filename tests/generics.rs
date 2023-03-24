337de14044faf5cb5f674d761a7f1aaf7351dfc9
Nathan Wiles <ntwiles@gmail.com>
2023-03-24 18:31:32 -0400

    Update TODO in generics tests.

--- tests/generics.rs
+++ tests/generics.rs
@@ -45,17 +45,12 @@ fn it_allows_generic_func_declare() {
     assert_eq!(ctx.get_output(), vec!["test"]);
 }
 
-// TODO: In the following example, T will be inferred to be type String (the type of 'Howard'). However,
-// we return a type Option<Number>, and no type checking is done to discover that error. This should be fixed.
-// func find<T>(haystack: Array<String>, needle: T ): T {
-//     for item in haystack {
-//         if (item == needle) {
-//             break(Option::Some(7));
-//         }
-//     }
-
-//     Option::None;
+// TODO: In the following example, `T` will be inferred to be type `String` (the type of `'test'`). However,
+// we return value `8` of type `Number`, and no type checking is done to catch that error. This should be fixed.
+//
+// func foo<T>( arg: T ): T {
+//     8;
 // }
 
-// const found = find([ 'Jimmy', 'Kim', 'Howard', 'Lalo' ], 'Howard');
+// const found = foo('test');
 // print(found);
